03374f206a40ff24a927b30db8a771f983b8bc68
DanyloKopytko <leonelluson@gmail.com>
2019-10-29 15:11:38 +0200

    Nothing special...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular1';
+  inputValue: string;
+  valueToFront: string;
+  status: number;
+  houses: object[] = [
+    {city: 'Lviv', meters: '100', price: '1000000', street: 'Gasd 23/7'},
+    {city: 'Kyiv', meters: '120', price: '1500000', street: 'Hilan 17/19'},
+    {city: 'Ivano-Frankivsk', meters: '90', price: '800000', street: 'Kolopita 3/21'},
+    {city: 'Lviv', meters: '105', price: '1200000', street: 'Hurika 1/1'},
+  ];
+
+  onInput(ev): void {
+    this.inputValue = ev.target.value;
+  }
+
+  search() {
+    this.valueToFront = this.inputValue;
+  }
+
+  renderLogin() {
+    this.status = 1;
+  }
+
+  renderRegister() {
+    this.status = 2;
+  }
 }
